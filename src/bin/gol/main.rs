cc342f2e120a1e9fea731a65b37a278760fc9e23
Akasaka Yosuke <yskaksk@gmail.com>
2022-06-21 23:20:08 +0900

    ライフゲーム実装した

---
+++ src/bin/gol/main.rs
@@ -0,0 +1,41 @@
+mod model;
+mod util;
+
+use model::Model;
+use winit::{
+    dpi::PhysicalSize,
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+fn main() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().with_inner_size(PhysicalSize::new(600.0, 600.0)).build(&event_loop).unwrap();
+
+    let mut model: Model = pollster::block_on(Model::new(&window));
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => {
+            match event {
+                WindowEvent::CloseRequested | WindowEvent::KeyboardInput { .. } => *control_flow = ControlFlow::Exit,
+                _ => {}
+            }
+        }
+        Event::RedrawRequested(_) => {
+            model.update();
+            match model.render() {
+                Ok(_) => {}
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                Err(e) => eprintln!("{:?}", e),
+            }
+        }
+        Event::RedrawEventsCleared => {
+            window.request_redraw();
+        }
+        _ => {}
+    });
+}
